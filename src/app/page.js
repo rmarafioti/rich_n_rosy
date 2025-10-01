7db7119cb6e737dd4b323abe7db1d9f58e098ec9
Rich Marafioti <richmarafioti@gmail.com>
2025-09-30 19:02:40 -0500

    #1 landing page edit

--- src/app/page.js
+++ src/app/page.js
@@ -6,32 +6,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1> Rich N Rosy Get Hitched</h1>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <h2> this is our wedding website</h2>
       </main>
       <footer className={styles.footer}>
         <a
