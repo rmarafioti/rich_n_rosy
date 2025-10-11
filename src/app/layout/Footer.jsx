0237f99ed4bfbf069b4c9f858fa2d174c8f01077
Rich Marafioti <richmarafioti@gmail.com>
2025-10-11 15:59:20 -0500

    #5 created navbar and footer jsx files

---
+++ src/app/layout/Footer.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>This is the footer</h1>
+    </footer>
+  );
+}
---
+++ src/app/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>This is the navbar</h1>
+    </nav>
+  );
+}
