bd129d6ace16350df20db7607da3c8794ac0a7ff
Rich Marafioti <richmarafioti@gmail.com>
2025-10-11 16:25:44 -0500

    #5 fleshed out navbar with menu and links

--- src/app/layout/Navbar.jsx
+++ src/app/layout/Navbar.jsx
@@ -1,9 +1,28 @@
 "use client";
+import Link from "next/link";
+import styles from "../styling/navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>This is the navbar</h1>
+      <h1 className={styles.icon}>*menu icon</h1>
+      <div className={styles.nav_menu}>
+        <Link href="" className={styles.nav_link}>
+          section one
+        </Link>
+        <Link href="" className={styles.nav_link}>
+          section two
+        </Link>
+        <Link href="" className={styles.nav_link}>
+          section three
+        </Link>
+        <Link href="" className={styles.nav_link}>
+          section four
+        </Link>
+        <Link href="" className={styles.nav_link}>
+          section five
+        </Link>
+      </div>
     </nav>
   );
 }
