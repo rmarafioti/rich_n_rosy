8777152dc4fe1275ad0431e273bc299a43d685f3
Rich Marafioti <richmarafioti@gmail.com>
2025-10-22 17:22:54 -0500

    #15 loaded in stylized font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
 import App_Wrapper from "./components/App_Wrapper";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Rosy & Rich Get Hitched",
@@ -21,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* fonts should be loaded in through app wrapper in order 
-      to remove font styles by way of access menu*/}
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <App_Wrapper>{children}</App_Wrapper>
       </body>
     </html>
