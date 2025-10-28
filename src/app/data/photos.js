d1a2cd6f314773fe440d997403d4612beea0c0d1
Rich Marafioti <richmarafioti@gmail.com>
2025-10-27 19:44:38 -0500

    #21 photos loeaded in data folder

---
+++ src/app/data/photos.js
@@ -0,0 +1,68 @@
+const engagement_photos = [
+  {
+    /* marquee photo */
+    id: 1,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611090/wedding_website/engagement_photos/marquee_close_web_czppmn.jpg",
+  },
+  {
+    /* walking with marquee */
+    id: 2,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761505752/wedding_website/engagement_photos/marquee_walking_web_y5mkhb.jpg",
+  },
+  {
+    /* close up with marquee */
+    id: 3,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611100/wedding_website/engagement_photos/marquee_kiss_three_web_ydeb1i.jpg",
+  },
+  {
+    /* full theatre */
+    id: 4,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611076/wedding_website/engagement_photos/full_theatre_web_ufacpr.jpg",
+  },
+  {
+    /* watching backs of heads */
+    id: 5,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761260843/wedding_website/engagement_photos/seats_watching_back_web_yidrxk.jpg",
+  },
+  {
+    /* seats kissing */
+    id: 6,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611199/wedding_website/engagement_photos/seats_kiss_two_web_qmlhuh.jpg",
+  },
+  {
+    /* ring close-up */
+    id: 7,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761612067/wedding_website/engagement_photos/ring_web_bl0urs.jpg",
+  },
+  {
+    /* aisle detail */
+    id: 8,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761257895/wedding_website/engagement_photos/aisle_detail_vzhyug.jpg",
+  },
+  {
+    /* theatre steps */
+    id: 9,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611225/wedding_website/engagement_photos/theatre_steps_two_web_mvhvqv.jpg",
+  },
+  {
+    /* walking aisle */
+    id: 10,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611252/wedding_website/engagement_photos/theatre_walking_two_web_vwbgzp.jpg",
+  },
+  {
+    /* walking aisle close up */
+    id: 11,
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1761611241/wedding_website/engagement_photos/theatre_walking_one_web_yrxamy.jpg",
+  },
+];
