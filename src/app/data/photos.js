421f1189ae463dc47354091d9c606949851a96d1
Rich Marafioti <richmarafioti@gmail.com>
2025-11-02 17:57:57 -0600

    #25 adjusted styling for nav bar reponsiveness and hero section

--- src/app/data/photos.js
+++ src/app/data/photos.js
@@ -1,3 +1,15 @@
+export const feature_photos = [
+  {
+    id: 1,
+    name: "watching_backs_of_heads",
+    photo:
+      "https://res.cloudinary.com/dzpne110u/image/upload/v1762127438/wedding_website/feature_photos/hero_section_x6c6sx.png",
+    alt: "engagement photo",
+    height: 1561,
+    width: 2912,
+  },
+];
+
 export const engagement_photos_pc = [
   {
     id: 1,
