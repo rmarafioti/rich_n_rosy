b8d7535047645565097726961e8f5285e089da47
Rich Marafioti <richmarafioti@gmail.com>
2025-11-02 20:33:27 -0600

    #25 new header styling implemented.

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,14 @@ export default function Home() {
       <div className={styles.header_container}>
         <h1 className={styles.title}>Rosy & Rich Get Hitched.</h1>
         <p className={styles.sub_title}>July 18th 2026</p>
+        <p className={styles.copy}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
       </div>
       {heroSectionPhotoPC && (
         <Image
