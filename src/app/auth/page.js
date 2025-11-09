6a204c950dbb054afe1fa52adf080a84b16d8fa6
Rich Marafioti <richmarafioti@gmail.com>
2025-11-09 08:38:01 -0600

    #29 auth page styled for pc, mobile light and dark mode

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -4,7 +4,11 @@ import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import ResponsiveImage from "../components/Responsive_Image";
-import { icons, feature_photos } from "../data/photos";
+import {
+  icons,
+  engagement_photos_mobile,
+  feature_photos,
+} from "../data/photos";
 
 import styles from "../styling/auth_page.module.css";
 
@@ -12,6 +16,7 @@ export default function Auth() {
   const icon = icons.find((p) => p.id === 2);
   const iconMobile = icons.find((p) => p.id === 1);
   const headerPhoto = feature_photos.find((p) => p.id === 5);
+  const headerPhotoMobile = engagement_photos_mobile.find((p) => p.id === 6);
 
   const [passcode, setPasscode] = useState("");
   const [loading, setLoading] = useState(false);
@@ -47,48 +52,55 @@ export default function Auth() {
 
   return (
     <main>
-      <div className={styles.header_container}>
-        <div className={styles.icon_container}>
-          <ResponsiveImage
-            pcPhoto={icon}
-            mobilePhoto={iconMobile}
-            pcClass={styles.icon}
-            mobileClass={styles.icon_mobile}
+      <article className={styles.content}>
+        <section className={styles.header_container}>
+          <Image
+            src={icon.photo}
+            alt={icon.alt}
+            height={icon.height}
+            width={icon.width}
+            className={styles.icon}
           />
+          <p className={styles.tag}>Rosy & Rich Get Hitched!</p>
           <p className={styles.date}>July &#8226; 18th &#8226; 2026</p>
-        </div>
-        <h1 className={styles.header}>Welcome!</h1>
-        <p className={styles.instructions}>
-          Please enter passcode to access website
-        </p>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div className={styles.input_section}>
-            <input
-              type="password"
-              value={passcode}
-              onChange={(e) => setPasscode(e.target.value)}
-              placeholder="Enter passcode"
-              disabled={loading}
-              className={styles.input}
-              required
-            />
-            <button type="submit" disabled={loading} className={styles.submit}>
-              {loading ? "Verifying..." : "Submit"}
-            </button>
-          </div>
-          {validationError.auth && (
-            <p className={styles.error_message}>
-              *Invalid passcode. Please try again.
-            </p>
-          )}
-        </form>
-      </div>
-      <Image
-        src={headerPhoto.photo}
-        alt={headerPhoto.alt}
-        width={headerPhoto.width}
-        height={headerPhoto.height}
-        className={styles.header_photo}
+        </section>
+        <section className={styles.passcode_container}>
+          <h1 className={styles.welcome}>Welcome!</h1>
+          <p className={styles.instructions}>
+            Please enter passcode to access website
+          </p>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.input_section}>
+              <input
+                type="password"
+                value={passcode}
+                onChange={(e) => setPasscode(e.target.value)}
+                placeholder="Enter passcode"
+                disabled={loading}
+                className={styles.input}
+                required
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.submit}
+              >
+                {loading ? "Verifying..." : "Submit"}
+              </button>
+            </div>
+            {validationError.auth && (
+              <p className={styles.error_message}>
+                *Invalid passcode. Please try again.
+              </p>
+            )}
+          </form>
+        </section>
+      </article>
+      <ResponsiveImage
+        pcPhoto={headerPhoto}
+        mobilePhoto={headerPhotoMobile}
+        pcClass={styles.header_photo}
+        mobileClass={styles.header_photo_mobile}
       />
     </main>
   );
