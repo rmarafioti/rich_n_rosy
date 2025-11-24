17c2f8177cc16e41a17373a0186578d7c1622a8b
Rich Marafioti <richmarafioti@gmail.com>
2025-11-23 22:24:31 -0600

    #41 renders our story text onto the screen

--- src/app/our_story/page.js
+++ src/app/our_story/page.js
@@ -1,9 +1,25 @@
 "use client";
 
+import { ourStory } from "../data/outStory";
+
+function StoryCard({ date, text }) {
+  return (
+    <div>
+      <p>{date}</p>
+      <p>{text}</p>
+    </div>
+  );
+}
+
 export default function section_three() {
   return (
     <main>
-      <h1>This is section three</h1>
+      <h1>Our Story</h1>
+      <div>
+        {ourStory.map((story) => (
+          <StoryCard key={story.id} date={story.date} text={story.text} />
+        ))}
+      </div>
     </main>
   );
 }
