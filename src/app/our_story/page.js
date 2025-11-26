ccdd87eb821b61d5c942fef13386c28c14eaace3
Rich Marafioti <richmarafioti@gmail.com>
2025-11-26 09:20:43 -0600

    #43 comments out our story mock up design

--- src/app/our_story/page.js
+++ src/app/our_story/page.js
@@ -30,7 +30,7 @@ export default function Our_Story() {
   return (
     <main className={styles.our_story_page}>
       <h1 className={styles.title}>Our Story</h1>
-      <div className={styles.story_card_container}>
+      {/*<div className={styles.story_card_container}>
         {ourStory.map((story, index) => (
           <StoryCard
             key={story.id}
@@ -46,7 +46,7 @@ export default function Our_Story() {
         width={ourStoryBackground.width}
         height={ourStoryBackground.height}
         className={styles.our_story_bg}
-      />
+      />*/}
     </main>
   );
 }
