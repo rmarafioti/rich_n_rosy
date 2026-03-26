06cf5bb8cb2188b4a4f1b530746ac0f92ae06147
Rich Marafioti <richmarafioti@gmail.com>
2026-03-26 10:25:48 -0500

    fix: loaded in all dual theme images for our story feature and tested
    Refs: #61

--- src/app/_data/ourStory.js
+++ src/app/_data/ourStory.js
@@ -17,91 +17,70 @@ export const ourStory = [
     date: "June 2021",
     text: "Our first date! It was a hot summer night and we spent hours on Rich's patio talking and getting to know each other. We knew right away that we were in for something special.",
   },
-  /*{
+  {
     id: 2,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724744/wedding_website/our_story/art_museum_maroon_pljan0.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809557/wedding_website/our_story/art_museum_blush_ibmulq.svg",
-    alt: "our story icon",
-    width: 138,
-    height: 138,
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527041/wedding_website/dual_theme_svgs/art_museum_h0dw0i.svg",
+    alt: "illustration of Rosy and Rich at the Milwaukee Art Museum",
+    width: 326,
+    height: 327,
     date: "August 2021",
     text: "Rich planned a romantic weekend getaway to Milwaukee. We visited the Milwaukee Art Museum and shared our mutual love for contemporary art. A particular painting, Zeta by Louis Morris, caught our eyes. The scale of the piece and the way the shapes and colors converged reminded us of the new love we were building and it quickly became one of our favorite pieces.",
   },
   {
     id: 3,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724798/wedding_website/our_story/ny_coffee_maroon_w7wwof.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809595/wedding_website/our_story/ny_coffee_blush_huinbv.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527109/wedding_website/dual_theme_svgs/ny_coffee_o3fsiv.svg",
     alt: "our story icon",
-    width: 127,
-    height: 137,
+    width: 303,
+    height: 324,
     date: "April 2022",
     text: "Rosy's first trip to New York! We enjoyed a boat ride in Central Park, incredible art museums, and a perfect New York slice in the city that shaped Rich.",
   },
   {
     id: 4,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724783/wedding_website/our_story/keys_maroon_bmqd4x.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809586/wedding_website/our_story/keys_blush_mpts3g.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527109/wedding_website/dual_theme_svgs/keys_rxl5uw.svg",
     alt: "our story icon",
-    width: 92,
-    height: 159,
+    width: 222,
+    height: 375,
     date: "August 2022",
     text: "We bought our dream home by the lake, a block away from Thorndale Beach, where we first said “I love you”. Since then, we've filled our home with color, vintage treasures, and so many joyful memories.",
   },
   {
     id: 5,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724834/wedding_website/our_story/the_boss_maroon_rsi85o.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809622/wedding_website/our_story/the_boss_blush_ijmlji.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527132/wedding_website/dual_theme_svgs/the_boss_uetjg7.svg",
     alt: "our story icon",
-    width: 137,
-    height: 167,
+    width: 325,
+    height: 398,
     date: "March 2023",
     text: "Rich's first Bruce Springsteen concert! Our relationship wouldn't be complete without Rich fully embracing The Boss.",
   },
   {
     id: 6,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724824/wedding_website/our_story/stevie_maroon_ip2jbr.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809613/wedding_website/our_story/stevie_blush_akd4nr.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527132/wedding_website/dual_theme_svgs/stevie_fyrvqd.svg",
     alt: "our story icon",
-    width: 93,
-    height: 162,
+    width: 224,
+    height: 382,
     date: "June 2024",
     text: "Our family became complete when we adopted Stevie from PAWS Chicago. Stevie was originally found as a stray, running the mean streets of Chicago, and has grown into the sweetest pup. He loves crunching on cucumbers, chasing his stuffed dinosaurs, and cuddling with his family (or anyone nearby). There is truly never a dull day with this little guy.",
   },
   {
     id: 7,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724765/wedding_website/our_story/bulb_maroon_btizuh.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809572/wedding_website/our_story/bulb_blush_qmxgos.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527070/wedding_website/dual_theme_svgs/bulb_lbvy3m.svg",
     alt: "our story icon",
-    width: 83,
-    height: 139,
+    width: 201,
+    height: 330,
     date: "2024/2025",
     text: "Lots of big changes! Rich closed his business, Wild Wind Tattoo, after 10 successful years to transition into a career in tech. Rosy continued her education and will receive her Master's in Learning Sciences right before the wedding. Owen became a pre-teen and discovered new hobbies and talents, in addition to making the honor roll. Stevie learned to sit and thrived at day care. Through it all, they supported each other as a family and grew closer than ever.",
   },
   {
     id: 8,
-    src_light:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764724808/wedding_website/our_story/ring_maroon_s832pq.svg",
-    src_dark:
-      "https://res.cloudinary.com/dzpne110u/image/upload/v1764809605/wedding_website/our_story/ring_blush_apa6f7.svg",
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1774527123/wedding_website/dual_theme_svgs/ring_ccmr8b.svg",
     alt: "our story icon",
-    width: 118,
-    height: 147,
+    width: 282,
+    height: 348,
     date: "August 2025",
     text: "We planned an impromptu weekend trip at the end of the summer to visit House on the Rock in Wisconsin. We began the trip at the Milwaukee Art Museum, enjoying new exhibits and seeing some old favorites. When we arrived at Zeta, Rich asked Rosy if this was her favorite one. She said yes and then she said yes! Rich proposed in front of this beautiful piece of artwork that meant so much to us.",
     dateTwo: "July 2026",
     textTwo:
       "We say “I do!”. We can't wait to celebrate with you as we begin the next chapter of our lives together.",
-  },*/
+  },
 ];
