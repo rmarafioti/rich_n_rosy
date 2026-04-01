49034068eac1306e15ed12dba09f53af086b4d2a
Rich Marafioti <richmarafioti@gmail.com>
2026-04-01 13:20:10 -0500

    feat: registry page filled in with copy and complete styling
    Refs: #61

--- src/app/registry/page.js
+++ src/app/registry/page.js
@@ -1,9 +1,23 @@
 "use client";
 
+import styles from "../_styling/wedding_registry.module.css";
+
 export default function Registry() {
   return (
     <main>
-      <h1>This is the registry page</h1>
+      <article className={styles.header_container}>
+        <h1 className={styles.title}>Wedding Registry</h1>
+        <p className={styles.sub_title}>
+          Your presence at our wedding is a present in itself!
+        </p>
+        <p className={styles.copy}>
+          {" "}
+          If you are looking for gift ideas, we have created a registry with our
+          wishlist. Thank you in advance for the love and generosity you have
+          shown us, whether through our registry or in your own way.
+        </p>
+        <button className={styles.button}>Wedding Registry</button>
+      </article>
     </main>
   );
 }
